46103b07bf73c231de45995fd1325a65eb13e9bf
Ahror Kayumov <ahrorbek_n@hotmail.com>
2020-03-26 09:44:29 +0500

    Add project files.

---
+++ ViewModels/ConvertFileViewModel.cs
@@ -0,0 +1,44 @@
+﻿using CustomCareConverter.Views;
+using Prism.Commands;
+using ReactiveUI;
+using System.Windows.Input;
+
+namespace CustomCareConverter.ViewModels
+{
+    public class ConvertFileViewModel : ReactiveObject
+    {
+        public ICommand ShowExportTreatment { get; private set; }
+        public ICommand ShowImportTreatment { get; private set; }
+        public ICommand ExportTreatment { get; }
+        public ICommand ImportTreatment { get; }
+
+        public ConvertFileViewModel()
+        {
+            ShowExportTreatment = new DelegateCommand(ShowExportView);
+
+            ShowImportTreatment = new DelegateCommand(ShowImportView);
+
+            ExportTreatment = ReactiveCommand.Create(() =>
+            {
+
+            });
+
+            ImportTreatment = ReactiveCommand.Create(() =>
+            {
+
+            });
+        }
+
+        private void ShowExportView()
+        {
+            ExportView view = new ExportView() { DataContext = this };
+            view.ShowDialog();
+        }
+
+        private void ShowImportView()
+        {
+            ImportView view = new ImportView() { DataContext = this };
+            view.ShowDialog();
+        }
+    }
+}
